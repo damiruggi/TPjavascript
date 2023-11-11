a1635a94ca5904b21736b7cc4e67cbcafa3e788f
Damian <damiruggi@gmail.com>
2023-11-11 11:33:20 -0300

    Cambie a 3 decimales la respuesta

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ class Dolar {
         this.venta = venta
     }
 }
-
 const dolar1 = new Dolar("Blue", 940, 960)
 const dolar2 = new Dolar("MEP", 872.87, 875.45)
 const dolar3 = new Dolar("Banco Nación", 347.50, 365.50)
@@ -15,8 +14,8 @@ const dolar5 = new Dolar("Tarjeta", 695, 731)
 const dolar6 = new Dolar("Turista", 695, 731)
 const dolar7 = new Dolar("Mayorista", 348.95, 349.95)
 const dolar8 = new Dolar("Futuro", 383.60, 383.90)
-const dolar9 = new Dolar("Cripto", 919.85, 888.131)
-const dolar10 = new Dolar("Ccl", 874.84, 881.09)
+const dolar9 = new Dolar("Cripto", 919.85, 888.13)
+const dolar10 = new Dolar("CCL", 874.84, 881.09)
 const dolares = [dolar1, dolar2, dolar3, dolar4, dolar5, dolar6, dolar7, dolar8, dolar9, dolar10]
 
 for(let i = 0; i < dolares.length; i ++) {
@@ -29,13 +28,13 @@ for(let i = 0; i < dolares.length; i ++) {
     let operacion = prompt("¿Desea comprar o vender dólares?")
     switch (operacion) {
         case "comprar":
-            const DolarCompra = alert(dolares.map(prod => " " + prod.title + ": " + " $" + prod.compra))
+            const DolarCompra = alert(dolares.map(prod => " " + prod.title + ": " + " $ " + prod.compra))
             function compra(monto, valor) {
             let montoPesos
             do {
                 montoPesos = parseFloat (prompt("Ingrese cuántos pesos va a utilizar para comprar"))
             } while (isNaN(montoPesos) == true)
-            const DolarCotizacionCompra = alert(dolares.map(prod => " " + prod.title + ": " + " $" + (montoPesos / prod.compra)))
+            const DolarCotizacionCompra = alert(dolares.map(prod => " " + prod.title + ": " + " USD " + (montoPesos / prod.compra).toFixed(3)))
             }
     
             let confirmacionCompra = true
@@ -52,7 +51,7 @@ for(let i = 0; i < dolares.length; i ++) {
             break
     
         case "vender":
-            const DolarVenta = alert(dolares.map(prod => " " + prod.title + ": " + " $" + prod.venta))
+            const DolarVenta = alert(dolares.map(prod => " " + prod.title + ": " + " $ " + prod.venta))
             function venta(monto, valor) {
             let montoDolares
             do {
